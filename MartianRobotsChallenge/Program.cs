fa021d18c8f9f2bfa9991f44bef5d12bccdf0a88
Tom Day <tomjday26@gmail.com>
2021-09-23 14:59:03 +0200

    Add project files.

---
+++ MartianRobotsChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MartianRobotsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The first part of the problem is to be able to insert
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
